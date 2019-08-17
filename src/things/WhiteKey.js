fcbe7232c39512cf2974c57e4e7c932df3ad8c13
Zachary Chavez <zpchavez@gmail.com>
2019-08-17 12:26:00 -0700

    Label notes with control keys

--- src/things/WhiteKey.js
+++ src/things/WhiteKey.js
@@ -1,24 +1,34 @@
+import colors from '../colors';
+
 export const KEY_WIDTH = 40;
 const KEY_HEIGHT = 150;
 
 class WhiteKey
 {
-  constructor(g, index) {
+  constructor(g, index, controlKey) {
     this.g = g;
-    this.drawKey(index)
+    this.drawKey(index, controlKey)
   }
 
-  drawKey(index)
+  drawKey(index, controlKey)
   {
     this.sprite = this.g.rectangle(
       KEY_WIDTH,
       KEY_HEIGHT,
-      '#FFFFFF',
-      '#000000',
+      colors.white,
+      colors.black,
       2
     );
     this.sprite.x = 90 + (KEY_WIDTH * index);
     this.sprite.y = 310;
+
+    this.g.text(
+      controlKey,
+      '12px monospace',
+      colors.black,
+      this.sprite.x + 15,
+      this.sprite.y + KEY_HEIGHT - 20
+    );
   }
 
   update() {
