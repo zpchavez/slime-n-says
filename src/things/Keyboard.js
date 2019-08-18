53770019c63c487a334bab4290ba957ddb76f22c
Zachary Chavez <zpchavez@gmail.com>
2019-08-17 17:24:48 -0700

    Generate list of diatonic notes in every key

--- src/things/Keyboard.js
+++ src/things/Keyboard.js
@@ -2,8 +2,11 @@ import getControls from '../controls';
 import WhiteKey from './WhiteKey';
 import BlackKey from './BlackKey';
 
-const notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+const naturals = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
 const sharps = ['C', 'D', 'F', 'G', 'A'];
+const notes = [
+  'C4', 'Cs4', 'D4', 'Ds4', 'E4', 'F4', 'Fs4', 'G4', 'Gs4', 'A4', 'As4', 'B4', 'C5'
+];
 const controlMap = {
   C4: 'A',
   Cs4: 'W',
@@ -30,7 +33,7 @@ class Keyboard
 
   initControls() {
     this.controls = getControls(this.g);
-    notes.forEach(note => {
+    naturals.forEach(note => {
       this.controls[`${note}4`].press = () => this.playNote(note, 4);
       if (sharps.indexOf(note) > -1) {
         this.controls[`${note}s4`].press = () => this.playNote(`${note}s`, 4);
@@ -45,10 +48,34 @@ class Keyboard
     this.keys[`${note}${octave}`].highlight();
   }
 
+  generateMelody(noteCount) {
+    const ionianSteps = [2, 2, 1, 2, 2, 2, 1];
+    const getDiatonicIndices = (startingIndex) => {
+      const indices = [startingIndex];
+      ionianSteps.forEach(step => {
+        indices.push((indices[indices.length - 1] + step) % 13)
+      })
+      return indices;
+    }
+    const keys = {};
+    notes.forEach((note, i) => {
+      if (note === 'C5') return;
+      keys[note.replace('4', '')] = getDiatonicIndices(i);
+    })
+    const randomKey = this.g.randomPick(Object.keys(keys));
+    const diatonicIndices = keys[randomKey];
+    const diatonicNotes = notes.filter((n, i) => diatonicIndices.indexOf(i) > -1);
+    // const firstNote = this.g.randomPick(diatonicNotes);
+
+    for (let i = 0; i < noteCount; i += 1) {
+      // const lastInterval =
+    }
+  }
+
   drawKeys() {
     this.keys = {};
 
-    notes.forEach((n, i) => {
+    naturals.forEach((n, i) => {
       const note = `${n}4`
       this.keys[note] = new WhiteKey(this.g, i, controlMap[note]);
     })
