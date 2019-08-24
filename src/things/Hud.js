15f6c72617ef8a7a253280bf284577e1f2279a9c
Zachary Chavez <zpchavez@gmail.com>
2019-08-24 09:40:09 -0700

    Don't pause until correct note when angry key played

--- src/things/Hud.js
+++ src/things/Hud.js
@@ -5,26 +5,17 @@ class Hud
   constructor(g) {
     this.g = g;
     this.textUtil = new TextUtil(g);
-    // this.drawText();
   }
 
   setText(text) {
+    if (this.text && this.text.content === text) {
+      // Test is the same. No need to do anything.
+      return;
+    }
     if (this.text) {
       this.textUtil.clear();
     }
     this.text = this.textUtil.centeredText(text, 24, '#000000', 0);
-    // this.drawText();
-  }
-
-  drawText() {
-    // this.text = this.textUtil.centeredText(' ', 24, '#000000', 0);
-    // this.text = this.g.text(
-    //   'TEST',
-    //   '24px monospace',
-    //   '#000000',
-    //   0,
-    //   0
-    // );
   }
 }
 
