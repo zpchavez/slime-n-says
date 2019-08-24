c8578074682626bcee6551f7d426beb087deacd0
Zachary Chavez <zpchavez@gmail.com>
2019-08-24 09:56:17 -0700

    Remove keyboard callbacks

--- src/states/game.js
+++ src/states/game.js
@@ -4,25 +4,6 @@ import Hud from '../things/Hud';
 export default (g) => {
   const hud = new Hud(g);
   const keyboard = new Keyboard(g, hud);
-
-  let melodyLength = 2;
-
-  const generateMelody = () => {
-    hud.setText('Listen');
-    keyboard.resetSlimes();
-    keyboard.generateMelody(melodyLength);
-  }
-
-  keyboard.setOnPlayerDone(() => {
-    melodyLength++;
-    setTimeout(
-      generateMelody,
-      2000
-    );
-  });
-
-  keyboard.setOnStart(generateMelody);
-
   return () => {
     keyboard.update();
   };
